3567c588fe8622b3466d8f9d1bc257f43e258d3a
fermer123 <fermer_525@mail.ru>
2024-02-25 16:34:12 +0500

    snackbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client';
 
 import GlobalStyle from '@app/styles/global';
 import Theme from '@app/styles/Theme';
+const theme: string = 'dark';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Theme>
+  <Theme theme={theme}>
     <GlobalStyle />
     123
   </Theme>,
